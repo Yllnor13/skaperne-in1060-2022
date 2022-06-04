9b5f95d242e2a6a563093c6736769be83df80555
chiull <62013184+chiull@users.noreply.github.com>
2022-06-04 20:33:39 +0200

    Update klokke.ino

--- klokke.ino
+++ klokke.ino
@@ -3,15 +3,25 @@ int groonlys = 11;
 int roodlys = 10;
 int gullys = 9;
 
-int blaaknapp = 8;
-int groonknapp = 7;
-int roodknapp = 6;
-int gulknapp = 5;
+int svar_blaa = 8;
+int svar_groon = 7;
+int svar_rood = 6;
+int svar_gul = 5;
 
-int blaaState = 0;
-int groonState = 0;
-int roodState = 0;
-int gulState = 0;
+int hjelp_blaa = A0;
+int hjelp_groon = A1;
+int hjelp_rood = A2;
+int hjelp_gul = A3;
+
+int kommer_blaa = 0;
+int kommer_groon = 0;
+int kommer_rood = 0;
+int kommer_gul = 0;
+
+int send_blaa = 0;
+int send_groon = 0;
+int send_rood = 0;
+int send_gul = 0;
 
 void setup() {
   pinMode(blaalys, OUTPUT);
@@ -19,10 +29,15 @@ void setup() {
   pinMode(roodlys, OUTPUT);
   pinMode(gullys, OUTPUT);
   
-  pinMode(blaaknapp, INPUT);
-  pinMode(groonknapp, INPUT);
-  pinMode(roodknapp, INPUT);
-  pinMode(gulknapp, INPUT);
+  pinMode(svar_blaa, INPUT);
+  pinMode(svar_groon, INPUT);
+  pinMode(svar_rood, INPUT);
+  pinMode(svar_gul, INPUT);
+
+  pinMode(hjelp_blaa, INPUT);
+  pinMode(hjelp_groon, INPUT);
+  pinMode(hjelp_rood, INPUT);
+  pinMode(hjelp_gul, INPUT);
 
   digitalWrite(blaalys, HIGH);
   digitalWrite(groonlys, HIGH);
@@ -31,21 +46,45 @@ void setup() {
 }
 
 void loop() {
-  blaaState = digitalRead(blaaknapp);
-  groonState = digitalRead(groonknapp);
-  roodState = digitalRead(roodknapp);
-  gulState = digitalRead(gulknapp);
+  kommer_blaa = digitalRead(svar_blaa);
+  kommer_groon = digitalRead(svar_groon);
+  kommer_rood = digitalRead(svar_rood);
+  kommer_gul = digitalRead(svar_gul);
+  
+  send_blaa = digitalRead(hjelp_blaa);
+  send_groon = digitalRead(hjelp_groon);
+  send_rood = digitalRead(hjelp_rood);
+  send_gul = digitalRead(hjelp_gul);
+  hjelpkommer();
+  sendhjelp();
+}
 
-  if(blaaState == HIGH){
+void hjelpkommer(){
+   if(kommer_blaa == HIGH){
     digitalWrite(blaalys, LOW);
   }
-  if(groonState == HIGH){
+  if(kommer_groon == HIGH){
     digitalWrite(groonlys, LOW);
   }
-  if(roodState == HIGH){
+  if(kommer_rood == HIGH){
     digitalWrite(roodlys, LOW);
   }
-  if(gulState == HIGH){
+  if(kommer_gul == HIGH){
     digitalWrite(gullys, LOW);
+  }
+}
+
+void sendhjelp(){
+  if(send_blaa == HIGH){
+    digitalWrite(blaalys, HIGH);
+  }
+  if(send_groon == HIGH){
+    digitalWrite(groonlys, HIGH);
+  }
+  if(send_rood == HIGH){
+    digitalWrite(roodlys, HIGH);
+  }
+  if(send_gul == HIGH){
+    digitalWrite(gullys, HIGH);
   }
 }
