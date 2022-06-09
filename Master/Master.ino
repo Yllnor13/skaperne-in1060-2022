cacd5993014fd38cf5d53e4a95b172736a73512d
chiull <62013184+chiull@users.noreply.github.com>
2022-06-09 17:09:11 +0200

    la til kommentarer

--- Master/Master.ino
+++ Master/Master.ino
@@ -1,40 +1,44 @@
-// Include the required Wire library for I2C<br>
 #include<Wire.h>
-int x = 5;
+
+int x = 5; //verdi som blir sendt
+
+//knapp relatert variabler
 int buttonPin = 12;
 int buttonState = 0;
+
+//variablelen som holder verdien slave gir
 int receivedValue = 0;
 
 void setup() {
-  // Start the I2C Bus as Master
   Serial.begin(9600);
   Wire.begin(); 
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  //Request 1 byte from Arduino at address 9
+  //spør om 1 byte fra address 9 (slave arduino)
   Wire.requestFrom(9, 1);
 
-  while (Wire.available()) { // slave may send less than requested
-    receivedValue = Wire.read(); // receive a byte as int
-    Serial.println(receivedValue);         // print the int
+  while (Wire.available()) { //slave kan sende mindre enn etterspurt
+    receivedValue = Wire.read();
+    Serial.println(receivedValue);
   }
   
   buttonState = digitalRead(buttonPin);
   if(buttonState == HIGH){
-    x = 0;  
+    x = 0; //dette gjør at lyset slår seg på i slave
   }else{
-    if(receivedValue == 5){
+    if(receivedValue == 5){ //dette slår ikke på noe led i slave
       x = 5;
     }
   }
   transmission();
 }
 
+//sending av data til addressen 9 (slave arduino)
 void transmission(){
-  Wire.beginTransmission(9); // transmit to device #9
-  Wire.write(x);              // sends x 
-  Wire.endTransmission();    // stop transmitting
+  Wire.beginTransmission(9);
+  Wire.write(x);
+  Wire.endTransmission();
   delay(100);
 }
